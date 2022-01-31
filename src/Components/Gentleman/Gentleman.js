467fad6d6962d5ce22452c92253d3fd0020fb4f2
Alberto Vidal <albertovidal@Albertos-MacBook-Pro.local>
2022-01-31 20:46:04 +0100

    :sparkles: added html to gentleman component

--- src/Components/Gentleman/Gentleman.js
+++ src/Components/Gentleman/Gentleman.js
@@ -1,3 +1,43 @@
-const Gentleman = () => {
-  return <div></div>;
+const Gentleman = ({
+  gentleman: {
+    id,
+    name,
+    status,
+    profession,
+    tweeter,
+    picture,
+    alternative,
+    text,
+    selected,
+  },
+}) => {
+  return (
+    <li class="gentleman">
+      <div class="gentleman__avatar-container">
+        <img
+          class="gentleman__avatar"
+          src="img/fary.jpg"
+          alt="The Fary pointing at you"
+        />
+        <span class="gentleman__initial">F</span>
+      </div>
+      <div class="gentleman__data-container">
+        <h2 class="gentleman__name">The Fary</h2>
+        <ul class="gentleman__data-list">
+          <li class="gentleman__data">
+            <span class="gentleman__data-label">Profession:</span>
+            Influencer
+          </li>
+          <li class="gentleman__data">
+            <span class="gentleman__data-label">Status:</span> RIP
+          </li>
+          <li class="gentleman__data">
+            <span class="gentleman__data-label">Twitter:</span> Pending
+          </li>
+        </ul>
+      </div>
+      <i class="icon gentleman__icon fas fa-check"></i>
+      <i class="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+    </li>
+  );
 };
